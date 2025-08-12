dcf8373ac471374b932868c2f116caa003120d06
Aniruddha-2007 <bhagadeaniruddha3@gmail.com>
2025-08-12 23:09:16 +0530

    TPO_Registration

--- app/screens/tpoRegistration/index.tsx
+++ app/screens/tpoRegistration/index.tsx
@@ -1,13 +1,65 @@
-import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import '../../../global.css';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Header from 'components/ui/Header';
+import AddButton from 'components/ui/addButton';
 
-export default function tpoRegistration() {
-  return (
-      <SafeAreaView>
-        <View className='items-center justify-center bg-[#1877F2] py-60 m-11 rounded-lg mt-60'>
-          <Text text-xl>TPO Registration</Text>
+const detailData = [
+    { title: 'Placement', year: '2024-25' },
+    { title: 'Internship', year: '2024-25' },
+    { title: 'Placement And Internship', year: '2024-25' },
+    { title: 'Internship + PPO', year: '2024-25' },
+    { title: 'Placement', year: '2024-25' },
+];
+
+const App = () => {
+    const router = useRouter();
+    return (
+
+
+        <View className=" flex-1 px-10 pt-12 bg-white py-20">
+            <Header title='Tpo Registration' mode='normal' />
+            <ScrollView style={{ marginTop: 10 }}>
+                {detailData.map((item, index) => (
+
+                    <View key={index} className=" border border-[#ccc] rounded-lg overflow-hidden mb-3">
+
+
+                        <View className="bg-[#1877F2] p-3 flex flex-row justify-between items-center">
+
+
+                            <Text className="text-white font-semibold text-base"> {item.title}</Text>
+
+
+                            <TouchableOpacity>
+                                <Ionicons name="trash-outline" size={20} color="white" />
+                            </TouchableOpacity>
+                        </View>
+
+
+
+                        <View className={"bg-white p-3 flex flex-row justify-between"}>
+
+
+                            <Text className={"font-[600]"}>Academic Year</Text>
+
+
+                            <Text className={"text-black"}>{item.year}</Text>
+
+                        </View>
+                    </View>
+                ))}
+            </ScrollView>
+
+            <AddButton />
+
+
+
         </View>
-      </SafeAreaView>
-  );
-}
+
+    );
+};
+
+export default App;
+
