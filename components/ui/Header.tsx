ffeb6608de96152d3ecded3c3e4de734138f64ae
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-12 07:39:10 +0530

    Add the cases to Header component

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,22 +1,29 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 import AntDesign from '@expo/vector-icons/AntDesign';
+
 type HeaderProps = {
   title: string;
+  mode?: 'normal' | 'close'; 
 };
 
-export default function Header({ title }: HeaderProps) {
+export default function Header({ title, mode = 'normal' }: HeaderProps) {
   const router = useRouter();
 
   return (
-    <View className="h-14 flex-row items-center bg-transparent  px-4">
+    <View className="h-14 flex-row items-center bg-transparent px-4">
       <TouchableOpacity
         onPress={() => router.back()}
         activeOpacity={0.6}
         className="p-2"
         accessibilityRole="button"
-        accessibilityLabel="Go back">
-        <AntDesign name="arrowleft" size={24} color="black" />
+        accessibilityLabel={mode === 'close' ? 'Close' : 'Go back'}
+      >
+        <AntDesign
+          name={mode === 'close' ? 'close' : 'arrowleft'}
+          size={24}
+          color="black"
+        />
       </TouchableOpacity>
       <Text className="ml-2 text-lg font-semibold text-gray-900">{title}</Text>
     </View>
