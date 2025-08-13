690730292eba7c793e2d4fa403282054c0ea5dde
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-13 20:13:19 +0530

    improve code for show the scheduled companies in Flatlist

--- app/screens/scheduledCompanies/index.tsx
+++ app/screens/scheduledCompanies/index.tsx
@@ -1,48 +1,81 @@
-import { View, Text } from "react-native";
-import Header from "components/ui/Header";
-import JobCard from "components/scheduledCompanies/card";
+import { View, FlatList } from 'react-native';
+import Header from 'components/ui/Header';
+import JobCard from 'components/scheduledCompanies/card';
 
 export default function Index() {
+  const jobs = [
+    {
+      id: '1',
+      logo: require('../../../assets/images/micro.png'),
+      company: 'Microsoft Corporation',
+      type: 'Placement',
+      mode: 'Regular',
+      date: '04-Aug-2025',
+    },
+    {
+      id: '2',
+      logo: require('../../../assets/images/amazon.png'),
+      company: 'Amazon',
+      type: 'Internship',
+      mode: 'Regular',
+      date: '04-Aug-2025',
+    },
+    {
+      id: '3',
+      logo: require('../../../assets/images/google.png'),
+      company: 'Google',
+      type: 'Placement',
+      mode: 'Regular',
+      date: '04-Aug-2025',
+    },
+    {
+      id: '4',
+      logo: require('../../../assets/images/google.png'),
+      company: 'Facebook',
+      type: 'Placement',
+      mode: 'Regular',
+      date: '04-Aug-2025',
+    },
+    {
+      id: '5',
+      logo: require('../../../assets/images/google.png'),
+      company: 'Microsoft Corporation',
+      type: 'Placement',
+      mode: 'Regular',
+      date: '04-Aug-2025',
+    },
+    {
+      id: '6',
+      logo: require('../../../assets/images/Cisco.png'),
+      company: 'Microsoft Corporation',
+      type: 'Placement',
+      mode: 'Regular',
+      date: '04-Aug-2025',
+    },
+  ];
+
   return (
-    <View className="flex-1 bg-white pt-[50px] px-4">
+    <View className="flex-1 bg-white px-4 pt-12">
       {/* Header */}
       <Header title="Scheduled Companies" />
-      <JobCard
-        logo={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1200px-Microsoft_logo.svg.png' }}
-        company="Microsoft corporation"
-        type="Placement"
-        mode="Regular"
-        date="04-Aug-2025"
-        onApply={() => alert("Applied successfully")}
-        onMore={() => alert("More details")}
+
+      <FlatList
+        data={jobs}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <JobCard
+            logo={item.logo}
+            company={item.company}
+            type={item.type}
+            mode={item.mode}
+            date={item.date}
+            onApply={() => alert('Applied successfully')}
+            onMore={() => alert('More details')}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20, paddingHorizontal:15 }}
       />
-      <JobCard
-        logo={{ uri: 'https://logos-world.net/wp-content/uploads/2020/06/Amazon-Logo.png' }}
-        company="Amazon"
-        type="Internship"
-        mode="Regular"
-        date="04-Aug-2025"
-        onApply={() => alert("Applied successfully")}
-        onMore={() => alert("More details")}
-      />
-        <JobCard
-            logo={{ uri: 'https://storage.googleapis.com/gd-prod/images/a910d418-7123-4bc4-aa3b-ef7e25e74ae6.60c498c559810aa0.webp' }}
-            company="Google"
-            type="Placement"
-            mode="Regular"
-            date="04-Aug-2025"
-            onApply={() => alert("Applied successfully")}
-            onMore={() => alert("More details")}
-        />
-        <JobCard
-            logo={{ uri: 'https://static.vecteezy.com/system/resources/thumbnails/018/930/698/small/facebook-logo-facebook-icon-transparent-free-png.png' }}
-            company="Facebook"
-            type="Placement"
-            mode="Regular"
-            date="04-Aug-2025"
-            onApply={() => alert("Applied successfully")}
-            onMore={() => alert("More details")}
-        />
     </View>
   );
 }
