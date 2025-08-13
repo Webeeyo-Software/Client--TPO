43f37452638ca911f2d2905da8c06310d6584cd8
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-13 21:51:03 +0530

    revert the index page of companies which is changed and delete by @kunal-Shejwal

---
+++ app/screens/companies/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <SafeAreaView>
+            <View className='items-center justify-center bg-[#1877F2] py-60 m-11 rounded-lg mt-60'>
+              <Text text-xl>Companies</Text>
+            </View>
+          </SafeAreaView>
+  )
+}
+
+export default index
