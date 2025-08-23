4c114a63ce92630e70e30757526a513c2113d51a
CodeWizAbhi <lohotabhishek16@gmail.com>
2025-08-23 11:54:19 +0530

    Adding reset password feature

--- app/screens/auth/resetPassScreen.tsx
+++ app/screens/auth/resetPassScreen.tsx
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
-import { View, ScrollView } from "react-native";
+import { View, Alert } from "react-native";
+import { useRouter, useLocalSearchParams } from "expo-router";
 import Header from "components/ui/Header";
 import InputField from "components/ui/InputField";
 import PrimaryButton from "components/ui/PrimaryButton";
-import { useRouter } from "expo-router";
 import Title from "components/auth/Login/Title";
+import { API_BASE_URL } from "utils/api";
+
 const ResetPasswordScreen: React.FC = () => {
   const router = useRouter();
+  const { email } = useLocalSearchParams<{ email: string }>();
+
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleSubmit = () => {
-    console.log("New Password:", newPassword);
-    console.log("Confirm Password:", confirmPassword);
-    router.push('screens/auth/verifyComplete');
+  const handleSubmit = async () => {
+    if (newPassword !== confirmPassword) {
+      Alert.alert("Error", "Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/resetpassword`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, newPassword }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        Alert.alert("Error", data.message || "Failed to reset password");
+        return;
+      }
+
+      Alert.alert("Success", "Password reset successful!");
+      router.push("/screens/auth/verifyComplete");
+    } catch (err) {
+      Alert.alert(`Error`, `Network error ${err}`,);
+    }
   };
 
   return (
     <View className="flex-1 bg-white px-4 pt-12">
-      <Title title1="Reset" title2="Password"/>
+      <Title title1="Reset" title2="Password" />
       <View className="mx-5 mt-5">
-        
         <InputField
           label="New Password"
           placeholder="Enter your password"
@@ -29,7 +53,6 @@ const ResetPasswordScreen: React.FC = () => {
           onChangeText={setNewPassword}
           required
         />
-
         <InputField
           label="Confirm new password"
           placeholder="Re-enter new password"
@@ -38,8 +61,8 @@ const ResetPasswordScreen: React.FC = () => {
           onChangeText={setConfirmPassword}
           required
         />
-        <View className="mt-96 pt-56">
-        <PrimaryButton label="Submit" onPress={handleSubmit} />
+        <View className="mt-20">
+          <PrimaryButton label="Submit" onPress={handleSubmit} />
         </View>
       </View>
     </View>
